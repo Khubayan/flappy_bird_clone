1be2fec1605dde0ef0a2ce7a8aa4617267429604
Khubayan <kubayan73@gmail.com>
2025-04-22 13:34:51 +0800

    Save Score

--- Scripts/Main/main.gd
+++ Scripts/Main/main.gd
@@ -1,13 +1,31 @@
 extends Node2D
 
+const SAVEFILE: String = "user://score.save"
+
 @onready var spawner: Spawner = $Spawner
 @onready var player: Player = $Player
 @onready var ground: Ground = $Ground
 @onready var ui: UI = $UI
 
 var score: int = 0
+var high_score: int = 0
 
-func _on_player_on_game_started() -> void:
+func _ready() -> void:
+	load_highscore()
+
+func load_highscore() -> void:
+	var file: FileAccess = FileAccess.open(SAVEFILE, FileAccess.READ)
+	if file:
+		high_score = file.get_32()
+
+func save_score() -> void:
+	if score > high_score:
+		var file: FileAccess = FileAccess.open(SAVEFILE, FileAccess.WRITE)
+		if file:
+			file.store_32(score)
+
+
+func _on_player_on_game_started( ) -> void:
 	spawner.timer.start()
 	ui.start_menu.hide()
 
@@ -28,8 +46,11 @@ func stop_gameplay() -> void:
 	player.stop_player_movement()
 	spawner.stop_obstacle() 
 	ground.speed = 0
-
+	load_highscore()
+	ui.game_over()
+	ui.calculate_score(score, high_score)
 
 func _on_spawner_on_player_scored() -> void:
 	score += 1
+	save_score()
 	ui.update_score(score)
--- Scripts/UI/ui.gd
+++ Scripts/UI/ui.gd
@@ -1,14 +1,41 @@
 extends CanvasLayer
 class_name  UI
 
-
-@onready var label: Label = %Label
+const MEDAL_BRONZE = preload("res://Assets/Sprites/UI/medalBronze.png")
+const MEDAL_GOLD = preload("res://Assets/Sprites/UI/medalGold.png")
+const MEDAL_SILVER = preload("res://Assets/Sprites/UI/medalSilver.png")
+@onready var medal_img: TextureRect = %"Medal IMG"
+@onready var current_score: Label = %CurrentScore
+@onready var high_score: Label = %HighScore
+@onready var score_label: Label = %ScoreLabel
 @onready var start_menu: Control = $MarginContainer/StartMenu
+@onready var game_over_container: VBoxContainer = %GameOver
 
 
 
 func _ready() -> void:
-	label.text = "0"
+	score_label.text = "0"
 
 func update_score(value: int) -> void:
-	label.text = str(value)
+	score_label.text = str(value)
+
+func game_over() -> void:
+	game_over_container.show()
+	score_label.hide()
+
+func calculate_score(current: int, high: int) -> void:
+	const MIN_GET_MEDAL_GOLD = 20
+	const MIN_GET_MEDAL_SILVER = 10
+	
+	current_score.text = str(current)
+	if current >= MIN_GET_MEDAL_GOLD:
+		medal_img.texture = MEDAL_GOLD
+	elif current >= MIN_GET_MEDAL_SILVER:
+		medal_img.texture = MEDAL_SILVER
+	else:
+		medal_img.texture = MEDAL_BRONZE
+	high_score.text = str(high)
+
+
+func _on_ok_button_pressed() -> void:
+	get_tree().reload_current_scene()
